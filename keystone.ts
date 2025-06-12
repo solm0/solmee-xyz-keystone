558286dae1f561051f2397b8349d1e6aa56ccbc9
정솔미 <solmi-@kookmin.ac.kr>
2025-06-12 19:57:13 +0200

    First commit

---
+++ keystone.ts
@@ -0,0 +1,22 @@
+import { config } from '@keystone-6/core'
+
+// to keep this file tidy, we define our schema in a different file
+import { lists } from './schema'
+
+// authentication is configured separately here too, but you might move this elsewhere
+// when you write your list-level access control functions, as they typically rely on session data
+import { withAuth, session } from './auth'
+
+export default withAuth(
+  config({
+    db: {
+      // we're using sqlite for the fastest startup experience
+      //   for more information on what database might be appropriate for you
+      //   see https://keystonejs.com/docs/guides/choosing-a-database#title
+      provider: 'sqlite',
+      url: 'file:./keystone.db',
+    },
+    lists,
+    session,
+  })
+)
