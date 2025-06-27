7ca87eac349ff6628c8a3ce743e8b61ec9b9c6cd
Solmi <js5185204@gmail.com>
2025-06-27 16:44:51 +0000

    document-internal link, link, backlink, order

--- schema.ts
+++ schema.ts
@@ -7,7 +7,8 @@ import {
   password,
   timestamp,
   select,
-  checkbox
+  checkbox,
+  integer,
 } from '@keystone-6/core/fields'
 
 import { document } from '@keystone-6/fields-document'
@@ -27,11 +28,6 @@ export const lists = {
         defaultValue: { kind: 'now' },
       }),
       isAdmin: checkbox(),
-
-      posts: relationship({
-        ref: 'Post.author',
-        many: true
-      }),
     },
   }),
 
@@ -41,7 +37,14 @@ export const lists = {
       title: text({ validation: { isRequired: true } }),
       publishedAt: timestamp(),
       content: document({
-        formatting: true,
+        formatting: {
+          inlineMarks: true,
+          listTypes: true,
+          alignment: true,
+          headingLevels: [2, 3, 4],
+          blockTypes: true,
+          softBreaks: true,
+        },
         links: true,
         dividers: true,
         layouts: [
@@ -52,23 +55,11 @@ export const lists = {
           [1, 2, 1],
         ],
         relationships: {
-          tag: {
-            listKey: 'Tag',
-            label: 'Tag',
-            selection: 'id name',
-          },
-        },
-      }),
-
-      author: relationship({
-        ref: 'User.posts',
-        many: false,
-        ui: {
-          displayMode: 'cards',
-          cardFields: ['name', 'email'],
-          inlineEdit: { fields: ['name', 'email'] },
-          linkToItem: true,
-          inlineConnect: true,
+          post: {
+            listKey: 'Post',
+            label: 'Internal Link',
+            selection: 'id title',
+          }
         },
       }),
 
@@ -83,9 +74,34 @@ export const lists = {
         },
       }),
 
+      links: relationship({
+        ref: 'Post.backlinks',
+        many: true,
+        ui: {
+          displayMode: 'cards',
+          cardFields: ['title', 'order'],
+          linkToItem: true,
+          inlineConnect: true,
+          inlineEdit: { fields: ['order'] },
+        }
+      }),
+
+      backlinks: relationship({
+        ref: 'Post.links',
+        many: true,
+        ui: {
+          displayMode: 'cards',
+          cardFields: ['title'],
+          linkToItem: true,
+          inlineConnect: false,
+        }
+      }),
+
       meta: checkbox({
         defaultValue: false,
       }),
+
+      order: integer(),
 
       status: select({
         options: [
@@ -98,23 +114,13 @@ export const lists = {
     },
   }),
 
-  // this last list is our Tag list, it only has a name field for now
   Tag: list({
-    // WARNING
-    //   for this starter project, anyone can create, query, update and delete anything
-    //   if you want to prevent random people on the internet from accessing your data,
-    //   you can find out more at https://keystonejs.com/docs/guides/auth-and-access-control
     access: allowAll,
-
-    // setting this to isHidden for the user interface prevents this list being visible in the Admin UI
     ui: {
       isHidden: true,
     },
-
-    // this is the fields for our Tag list
     fields: {
       name: text(),
-      // this can be helpful to find out all the Posts associated with a Tag
       posts: relationship({ ref: 'Post.tags', many: true }),
     },
   }),
